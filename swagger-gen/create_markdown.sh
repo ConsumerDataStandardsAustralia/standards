9b8d7e46f5314c485fa5026902f44612dc3820de
kirky61 <brian.kirkpatrick@data61.csiro.au>
2020-09-25 18:29:12 +1000

    Update for error in 1.5.0 archive

--- swagger-gen/create_markdown.sh
+++ swagger-gen/create_markdown.sh
@@ -9,10 +9,6 @@ diff -w cds_full.md ../slate/source/includes/cds_full.md > diff.txt
 node ./widdershins-cdr/widdershins.js --environment ./widdershins-cdr/cdr_widdershins.json --search false --language_tabs 'http:HTTP' 'javascript:Javascript' --summary api/cds_admin.json -o cds_admin.md
 # TODO Fix
 # Hack to clean up markdown for slate. Widdershins output does not support more than one swagger file markdown
-#sed -i '' '/consumer-data-standards-administration-end-points-admin-apis/d' "cds_admin.md"
-#sed -i '' 's/# Schemas/## Schemas/g' "cds_admin.md"
-#sed -i '' 's/This operation does not require authentication/This operation may only be called by the CDR Register/g' "cds_admin.md"
-
 sed -i  '/consumer-data-standards-administration-end-points-admin-apis/d' cds_admin.md
 sed -i  's/# Schemas/## Schemas/g' cds_admin.md
 sed -i  's/This operation does not require authentication/This operation may only be called by the CDR Register/g' cds_admin.md
