f381ce8b4f1c579d76eb0723117347451a0b6165
Nils Berge <60594671+nils-work@users.noreply.github.com>
2024-07-25 14:15:31 +1000

    Updated to newer openapi-generator-cli
    Added new jar and removed older jars

--- swagger-gen/oas_generate.sh
+++ swagger-gen/oas_generate.sh
@@ -31,12 +31,12 @@ mkdir $SWAGGER_CODEGEN_OUTPUT
 
 #codegen validator
 echo "*** Checking OAS Validator ***"
-VALID_OAS=$(java -jar $OAS_CODEGEN/openapi-generator-cli.jar validate -i $INPUT_SWAGGER)
+VALID_OAS=$(java -jar $OAS_CODEGEN/openapi-generator-cli-6.6.0.jar validate -i $INPUT_SWAGGER)
 echo "*** OAS Validator: " $VALID_OAS
 
 # generate
 echo "*** Generating $OUTPUT_FORMAT"
-java -jar $OAS_CODEGEN/openapi-generator-cli-5.2.1.jar generate -i $INPUT_SWAGGER -g $OUTPUT_FORMAT -o $SWAGGER_CODEGEN_OUTPUT > $SWAGGER_CODEGEN_OUTPUT/log.txt 2>&1
+java -jar $OAS_CODEGEN/openapi-generator-cli-6.6.0.jar generate -i $INPUT_SWAGGER -g $OUTPUT_FORMAT -o $SWAGGER_CODEGEN_OUTPUT > $SWAGGER_CODEGEN_OUTPUT/log.txt 2>&1
 
 echo `cat $SWAGGER_CODEGEN_OUTPUT/README.md`
 
